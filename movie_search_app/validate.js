2c4666f67841fc0a017e71df1bdf27b8e41f3c6f
himath <himathcode100@gmail.com>
2025-01-04 20:43:05 +0530

    modify valid.js

--- movie_search_app/validate.js
+++ movie_search_app/validate.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded",function(){
-    let myvalue=document.querySelector("#validemail").value
-    if(!myvalue.includes("@")){
-        let newelament=document.createElement("span")
-        newelament.textContent="Not a valid email"
-        newelament.style.color="red";
-        document.querySelector("#newx").appendChild(newelament)
-
-    }
+    document.addEventListener("onsubmit",function(){
+        let myvalue=document.querySelector("#validemail").value
+        if(!myvalue.includes("@")){
+            let newelament=document.createElement("span")
+            newelament.textContent="Not a valid email"
+            newelament.style.color="red";
+            document.querySelector("#newx").appendChild(newelament)
+            return false
+    
+        }
+    })
     
 })
